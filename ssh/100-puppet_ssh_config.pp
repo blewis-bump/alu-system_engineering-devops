42eed3e1a01ca33df5377d5c65010c9101e9fb77
blewis-bump <b.lewis@alustudent.com>
2026-07-20 06:57:59 -0700

    Add puppet ssh config manifest

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 #Configures the SSH client to use ~/.ssh/school as the identity file
 #xand refuse password authentication.
 file_line { 'Declare identity file':
-  path => '/root/.ssh/config',
+  path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Turn off passwd auth':
-  path => '/root/.ssh/config',
+  path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
